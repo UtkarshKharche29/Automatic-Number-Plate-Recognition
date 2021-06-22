7c0166d260b684938f9468941a0679ba8633af2b
UKROCKZ <utkarshkharche29101998@gmail.com>
2021-06-22 10:03:51 +0530

    add new files

---
+++ demo.py
@@ -0,0 +1,202 @@
+
+# import sys
+# import time
+# from PIL import Image, ImageDraw
+# from models.tiny_yolo import TinyYoloNet
+from csv import writer
+import pandas as pd
+from tool.utils import *
+from tool.torch_utils import *
+from tool.darknet2pytorch import Darknet
+import argparse
+import numpy as np
+from stage2.segment import *
+from PIL import Image
+import pytesseract
+
+
+"""hyper parameters"""
+if torch.cuda.is_available():
+    use_cuda = True
+else:
+    use_cuda = False
+def detect_cv2(cfgfile, weightfile, imgfile):
+    import cv2
+    m = Darknet(cfgfile)
+
+    m.print_network()
+    m.load_weights(weightfile)
+    print('Loading weights from %s... Done!' % (weightfile))
+
+    if use_cuda:
+        m.cuda()
+
+    num_classes = m.num_classes
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/x.names'
+    class_names = load_class_names(namesfile)
+
+    img = cv2.imread(imgfile)
+    sized = cv2.resize(img, (m.width, m.height))
+    sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+    for i in range(2):
+        start = time.time()
+        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
+        finish = time.time()
+        if i == 1:
+            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
+    project_path = os.getcwd()
+
+    if os.path.exists(".\preds\predictions.jpg") and os.path.exists(".\preds\gray.png") :
+        os.remove('.\preds\predictions.jpg')
+        os.remove('.\preds\gray.png')
+    result = plot_boxes_cv2(img, boxes[0], savename='preds\predictions.jpg', class_names=class_names)
+    result = cv2.resize(result,(int(600),int(600)))
+    cv2.imshow("Bounding Box", result)
+    cv2.imwrite(".\preds\bbox.jpg", result)
+    cv2.waitKey(0)
+    if os.path.exists('.\preds\predictions.jpg'):
+        #segmentation('.\preds\predictions.jpg')image = cv2.imread(args["image"])
+        img = cv2.imread('.\preds\predictions.jpg')
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        if args.p == "thresh":
+            rect,gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        elif args.p == "blur":
+            gray = cv2.medianBlur(gray, 3)
+
+        cv2.imwrite('.\preds\gray.png', gray)
+        text = pytesseract.image_to_string(Image.open('.\preds\gray.png'))
+        if not os.path.exists(".\data.csv"):
+            raw_data = {'date':[time.asctime( time.localtime(time.time()))],'Number Plate':[text]}
+            df = pd.DataFrame(raw_data)
+            df.to_csv('data.csv',mode='a')
+        else :
+            df=pd.read_csv('.\data.csv')
+            df.loc[len(df.index)]=[len(df.index), time.asctime( time.localtime(time.time())), text]
+            df.to_csv('data.csv',mode='w',index=False)
+        #    with open(".\data.csv", 'a+', newline='') as write_obj:
+        #        csv_writer = writer(write_obj)
+        #        csv_writer.writerow([time.asctime( time.localtime(time.time())), text])
+        os.remove('.\preds\gray.png')
+        print(text)
+        cv2.imshow("Image", img)
+        cv2.imshow("Output", gray)
+        cv2.waitKey(0)
+
+
+
+
+def detect_cv2_camera(cfgfile, weightfile):
+    args=get_args()
+    import cv2
+    m = Darknet(cfgfile)
+
+    m.print_network()
+    m.load_weights(weightfile)
+    print('Loading weights from %s... Done!' % (weightfile))
+
+    if use_cuda:
+        m.cuda()
+
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("./output.mp4")
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    print("Starting the YOLO loop...")
+
+    num_classes = m.num_classes
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/x.names'
+    class_names = load_class_names(namesfile)
+    frame_count = 0
+    while True:
+        ret, img = cap.read()
+        sized = cv2.resize(img, (m.width, m.height))
+        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+        #start = time.time()
+        boxes = do_detect(m, sized, 0.4, 0.6, use_cuda)
+        #finish = time.time()
+        #print('Predicted in %f seconds.' % (finish - start))
+
+        result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=class_names)
+
+        cv2.imshow('Yolo demo', result_img)
+        if np.any(img!=result_img):
+            cv2.imwrite("./frames/frame{}.png".format(frame_count), result_img)
+            frame_count+=1
+        cv2.waitKey(1)
+
+    cap.release()
+
+
+def detect_skimage(cfgfile, weightfile, imgfile):
+    from skimage import io
+    from skimage.transform import resize
+    m = Darknet(cfgfile)
+
+    m.print_network()
+    m.load_weights(weightfile)
+    print('Loading weights from %s... Done!' % (weightfile))
+
+    if use_cuda:
+        m.cuda()
+
+    num_classes = m.num_classes
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/x.names'
+    class_names = load_class_names(['Cars', 'Plate'])
+
+    img = io.imread(imgfile)
+    sized = resize(img, (m.width, m.height)) * 255
+
+    for i in range(2):
+        start = time.time()
+        boxes = do_detect(m, sized, 0.4, 0.4, use_cuda)
+        finish = time.time()
+        if i == 1:
+            print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
+
+    plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser('Test your image or video by trained model.')
+    parser.add_argument('-cfgfile', type=str, default='./cfg/darknet-yolov3.cfg',
+                        help='path of cfg file', dest='cfgfile')
+    parser.add_argument('-weightfile', type=str,
+                        default='./weight_folder/lapi.weights',
+                        help='path of trained model.', dest='weightfile')
+    parser.add_argument('-imgfile', type=str,
+                        default=None,
+                        help='path of your image file.', dest='imgfile')
+    parser.add_argument("-p", "--preprocess",dest='p' ,type=str, default="thresh",
+	                   help="type of preprocessing to be done")
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    if args.imgfile:
+        detect_cv2(args.cfgfile, args.weightfile, args.imgfile)
+        # detect_imges(args.cfgfile, args.weightfile)
+        # detect_cv2(args.cfgfile, args.weightfile, args.imgfile)
+        # detect_skimage(args.cfgfile, args.weightfile, args.imgfile)
+    else:
+        detect_cv2_camera(args.cfgfile, args.weightfile)
