4e88a84f3c27a6a771908b5f9cabbc1152475a15
Amansingh1202 <aks001235@gmail.com>
2021-06-22 17:45:51 +0530

    decreased window close time

--- demo.py
+++ demo.py
@@ -20,6 +20,8 @@ if torch.cuda.is_available():
     use_cuda = True
 else:
     use_cuda = False
+
+
 def detect_cv2(cfgfile, weightfile, imgfile):
     import cv2
     m = Darknet(cfgfile)
@@ -52,47 +54,46 @@ def detect_cv2(cfgfile, weightfile, imgfile):
             print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
     project_path = os.getcwd()
 
-    if os.path.exists(".\preds\predictions.jpg") and os.path.exists(".\preds\gray.png") :
+    if os.path.exists(".\preds\predictions.jpg") and os.path.exists(".\preds\gray.png"):
         os.remove('.\preds\predictions.jpg')
         os.remove('.\preds\gray.png')
-    result = plot_boxes_cv2(img, boxes[0], savename='preds\predictions.jpg', class_names=class_names)
-    result = cv2.resize(result,(int(600),int(600)))
+    result = plot_boxes_cv2(
+        img, boxes[0], savename='preds\predictions.jpg', class_names=class_names)
+    result = cv2.resize(result, (int(600), int(600)))
     cv2.imshow("Bounding Box", result)
     cv2.imwrite(".\preds\bbox.jpg", result)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     if os.path.exists('.\preds\predictions.jpg'):
-        #segmentation('.\preds\predictions.jpg')image = cv2.imread(args["image"])
+        # segmentation('.\preds\predictions.jpg')image = cv2.imread(args["image"])
         img = cv2.imread('.\preds\predictions.jpg')
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         if args.p == "thresh":
-            rect,gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+            rect, gray = cv2.threshold(
+                gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         elif args.p == "blur":
             gray = cv2.medianBlur(gray, 3)
 
         cv2.imwrite('.\preds\gray.png', gray)
         text = pytesseract.image_to_string(Image.open('.\preds\gray.png'))
         if not os.path.exists(".\data.csv"):
-            raw_data = {'date':[time.asctime( time.localtime(time.time()))],'Number Plate':[text]}
+            raw_data = {'date': [time.asctime(
+                time.localtime(time.time()))], 'Number Plate': [text]}
             df = pd.DataFrame(raw_data)
-            df.to_csv('data.csv',mode='a')
-        else :
-            df=pd.read_csv('.\data.csv')
-            df.loc[len(df.index)]=[len(df.index), time.asctime( time.localtime(time.time())), text]
-            df.to_csv('data.csv',mode='w',index=False)
-        #    with open(".\data.csv", 'a+', newline='') as write_obj:
-        #        csv_writer = writer(write_obj)
-        #        csv_writer.writerow([time.asctime( time.localtime(time.time())), text])
+            df.to_csv('data.csv', mode='a')
+        else:
+            df = pd.read_csv('.\data.csv')
+            df.loc[len(df.index)] = [len(df.index), time.asctime(
+                time.localtime(time.time())), text]
+            df.to_csv('data.csv', mode='w', index=False)
         os.remove('.\preds\gray.png')
         print(text)
         cv2.imshow("Image", img)
         cv2.imshow("Output", gray)
-        cv2.waitKey(0)
-
-
+        cv2.waitKey(1)
 
 
 def detect_cv2_camera(cfgfile, weightfile):
-    args=get_args()
+    args = get_args()
     import cv2
     m = Darknet(cfgfile)
 
@@ -128,12 +129,13 @@ def detect_cv2_camera(cfgfile, weightfile):
         #finish = time.time()
         #print('Predicted in %f seconds.' % (finish - start))
 
-        result_img = plot_boxes_cv2(img, boxes[0], savename=None, class_names=class_names)
+        result_img = plot_boxes_cv2(
+            img, boxes[0], savename=None, class_names=class_names)
 
         cv2.imshow('Yolo demo', result_img)
-        if np.any(img!=result_img):
+        if np.any(img != result_img):
             cv2.imwrite("./frames/frame{}.png".format(frame_count), result_img)
-            frame_count+=1
+            frame_count += 1
         cv2.waitKey(1)
 
     cap.release()
@@ -170,12 +172,13 @@ def detect_skimage(cfgfile, weightfile, imgfile):
         if i == 1:
             print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
 
-    plot_boxes_cv2(img, boxes, savename='predictions.jpg', class_names=class_names)
-
+    plot_boxes_cv2(img, boxes, savename='predictions.jpg',
+                   class_names=class_names)
 
 
 def get_args():
-    parser = argparse.ArgumentParser('Test your image or video by trained model.')
+    parser = argparse.ArgumentParser(
+        'Test your image or video by trained model.')
     parser.add_argument('-cfgfile', type=str, default='./cfg/darknet-yolov3.cfg',
                         help='path of cfg file', dest='cfgfile')
     parser.add_argument('-weightfile', type=str,
@@ -184,8 +187,8 @@ def get_args():
     parser.add_argument('-imgfile', type=str,
                         default=None,
                         help='path of your image file.', dest='imgfile')
-    parser.add_argument("-p", "--preprocess",dest='p' ,type=str, default="thresh",
-	                   help="type of preprocessing to be done")
+    parser.add_argument("-p", "--preprocess", dest='p', type=str, default="thresh",
+                        help="type of preprocessing to be done")
     args = parser.parse_args()
 
     return args
